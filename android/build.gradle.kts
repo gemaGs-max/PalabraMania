d16e11dea39d9d8730604d40bfd42fee8f0f0ba5
gemaGs-max <gemaotto@gmail.com>
2025-04-29 08:41:04 +0200

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.api.tasks.compile.JavaCompile
 
-// ⚠️ Si es build.gradle.kts (Kotlin DSL), este bloque va solo si usas dependencias antiguas.
+// Si es build.gradle.kts (Kotlin DSL), este bloque va solo si usas dependencias antiguas.
 buildscript {
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        // 👇 Plugin de servicios de Google para Firebase
+        //  Plugin de servicios de Google para Firebase
         classpath("com.google.gms:google-services:4.3.15")
     }
 }
@@ -20,11 +20,11 @@ allprojects {
     }
 }
 
-// 👇 Cambia la ruta de build para evitar conflictos de estructura
+//  Cambia la ruta de build para evitar conflictos de estructura
 val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.set(newBuildDir)
 
-// 👇 Configura los subproyectos
+//  Configura los subproyectos
 subprojects {
     val newSubprojectBuildDir = newBuildDir.dir(project.name)
     project.layout.buildDirectory.set(newSubprojectBuildDir)
@@ -43,7 +43,7 @@ subprojects {
     }
 }
 
-// 👇 Tarea para limpiar
+//  Tarea para limpiar
 tasks.register("clean", Delete::class) {
     delete(rootProject.layout.buildDirectory)
 }
